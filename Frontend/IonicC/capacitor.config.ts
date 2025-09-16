917192a6265c1e5c02a3276bd34ec12fd945ba80
MorenoBello1 <arielmorenobellojeampierre@gmail.com>
2025-09-16 15:47:41 -0500

    Agregar frontend al proyecto junto con backend

---
+++ Frontend/IonicC/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'IonicC',
+  webDir: 'www'
+};
+
+export default config;
