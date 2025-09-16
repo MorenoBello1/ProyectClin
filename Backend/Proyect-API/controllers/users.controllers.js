9e2d2b26a5c11f32d28d859cefa4b1d5b06c5ea7
MorenoBello1 <84943053+MorenoBello1@users.noreply.github.com>
2025-09-16 15:48:46 -0500

    Delete Backend/Proyect-API directory

--- Backend/Proyect-API/controllers/users.controllers.js
+++
@@ -1,7 +0,0 @@
-//user controladores
-exports.getAllUsers = (req, res) => {
-  res.json({mensaje:'Lista de usuarios'});
-};
-exports.Post = (req, res) => {
-  res.json({mensaje:'Lista de usuarios22'});
-};
--- Backend/Proyect-API/routers/login.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controladorLogin = require("../controllers/login.controllers")
-
-router.post('/',controladorLogin.Login)
-module.exports = router
